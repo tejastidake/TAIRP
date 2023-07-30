4c62e82888554bdfebe24b6cb41715f7336e0529
Tejas Tidake <tidaketejas2004@gmail.com>
2023-07-30 18:11:15 +0530

    Initial commit: Added Android Project Files

---
+++ TaskList/app/src/main/java/com/tejas/tasklist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tejas.tasklist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
