dccdb45e7f7afa6f962da49320cedb324707ab9e
Tejas Tidake <tidaketejas2004@gmail.com>
2023-08-03 13:33:50 +0530

    Getting Location Permission, Latitude and Longitude

--- ForecastZen/app/src/main/java/com/tejas/forecastzen/MainActivity.kt
+++ ForecastZen/app/src/main/java/com/tejas/forecastzen/MainActivity.kt
@@ -1,24 +1,31 @@
 package com.tejas.forecastzen
 
-import android.content.res.Configuration
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
+import android.location.Location
+import android.location.LocationManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.method.TextKeyListener.clear
+import android.os.Handler
+import android.provider.Settings
 import android.view.View
-import android.view.WindowManager
-import android.widget.TextClock
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.content.ContextCompat
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.tejas.forecastzen.databinding.ActivityMainBinding
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -29,6 +36,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.hide()
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
 
         val currentDateTime = Calendar.getInstance().time
@@ -72,6 +81,113 @@ class MainActivity : AppCompatActivity() {
 //                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             }
         }
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        getCurrentLocation()
+    }
+
+
+    private fun getCurrentLocation() {
+
+        if (checkPermissions())
+        {
+            if (isLocationEnabled())
+            {
+//                Toast.makeText(this, "Location Enabled", Toast.LENGTH_SHORT).show()
+
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    requestPermission()
+                    return
+                }
+
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this){ task->
+                    val location: Location?=task.result
+                    if (location==null)
+                    {
+                        Toast.makeText(this, "Loading...", Toast.LENGTH_LONG).show()
+                        Handler().postDelayed({getCurrentLocation()},4000)
+                    }
+                    else
+                    {
+                        Toast.makeText(this, "Latitude: ${location.latitude}", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "Longitude: ${location.longitude}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            else
+            {
+                Toast.makeText(this, "Turn On Location", Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        }
+
+        else
+        {
+            Toast.makeText(this, "Allow Access to Location", Toast.LENGTH_SHORT).show()
+            requestPermission()
+        }
+
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_REQUEST_ACCESS_LOCATION)
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun checkPermissions(): Boolean
+    {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)== PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED)
+        {
+            return true
+        }
+        return false
+    }
+
+
+    companion object
+    {
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode== PERMISSION_REQUEST_ACCESS_LOCATION)
+        {
+            if (grantResults.isNotEmpty() && grantResults[0]== PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(applicationContext, "Permission Granted", Toast.LENGTH_SHORT).show()
+//                getCurrentLocation()
+
+            }
+            else
+            {
+                Toast.makeText(applicationContext, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 
 }
